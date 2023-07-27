c419fe2f83673d2cc39ac79ade750677027e0205
Jonathan <jonathan.riffo7@gmail.com>
2023-07-27 12:34:10 -0300

    Implementacion de cards con mis proyectos

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 Rails.application.routes.draw do
   get '/home', to: 'pages#home'
-  get '/projects', to: 'pages#projects'
-  get '/contact', to: 'pages#contact'
+  get '/projectos', to: 'pages#projects'
+  get '/contacto', to: 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
